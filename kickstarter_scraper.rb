0cc3c31df4d47fd20bf188d441ded215f9c7d3cd
Joy Lin <vvlnote@hotmail.com>
2019-03-30 16:19:32 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,9 @@
 # require libraries/modules here
+require "nokogiri"
 
 def create_project_hash
   # write your code here
+  html = File.read("fixtures/kickstarter.html")
+  kickstarter = Nokogiri::HTML(html)
+  puts kickstarter.css('li.project.grid_4')[0]
 end
