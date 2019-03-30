247a4846d558749ee16870c63e21a49f8077423a
Joy Lin <vvlnote@hotmail.com>
2019-03-30 17:22:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ def create_project_hash
   # write your code here
   html = File.read("fixtures/kickstarter.html")
   kickstarter = Nokogiri::HTML(html)
-  #puts "#{kickstarter.css('li.project.grid_4').length}"
+  #puts "#{kickstarter.css('li.project.grid_4')}"
   projects = {}
   arr = kickstarter.css('li.project.grid_4')
   #puts "#{arr.length}"
