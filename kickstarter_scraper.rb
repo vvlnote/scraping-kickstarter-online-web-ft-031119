619fd34fabfb71fcde17a3de7e83d09cd9f568e7
Joy Lin <vvlnote@hotmail.com>
2019-03-30 17:15:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   puts "#{kickstarter.css('li.project.grid_4')[2]}"
   projects = {}
-  kickstarter.css('li.project.grid_4').collect do |project|
+  kickstarter.css('li.project.grid_4').each do |project|
     title = project.css("h2.bbcard_name strong a").text
     puts "title = #{title}"
     projects[title][:image_link] = project.css("div.project-thumbnail a img").attribute("src").value
