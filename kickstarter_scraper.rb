6c4107ae75982c9f10a1c86673be94f3d04cd5a2
Joy Lin <vvlnote@hotmail.com>
2019-03-30 17:37:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,10 +14,10 @@ def create_project_hash
     #puts 
     title = project.css("h2.bbcard_name strong a").text
     #puts "title = #{title}"
-    projects[title][:image_link] = project.css("div.project-thumbnail a img").attribute("src").value
+    projects[title.to-sym] = {[:image_link] = project.css("div.project-thumbnail a img").attribute("src").value
     projects[title][:description] = project.css("p.bbcard_blurb").text
     projects[title][:location] = project.css("span.location_name").text
-    projects[title][:percent_funded] = project.css("ul.project-stats li.first funded strong").text
+    projects[title][:percent_funded] = project.css("ul.project-stats li.first funded strong").text}
     #puts "projects"
   end
   projects
